06b16fd8deb779c776c52ede3dd6ef8b2206e4b6
WY <845283528@qq.com>
2018-07-11 11:46:00 +0800

    2

--- app/src/main/java/com/wy/arouterdemo/MainActivity.java
+++ app/src/main/java/com/wy/arouterdemo/MainActivity.java
@@ -74,19 +74,20 @@ public class MainActivity extends AppCompatActivity {
     @Autowired(name = "/service/hello")
     HelloService helloService1;
 
-
     /**
      * 服务
      * @param view
      */
     public void testService(View view){
+        // 1. (推荐)使用依赖注入的方式发现服务,通过注解标注字段,即可使用，无需主动获取
+        // Autowired注解中标注name之后，将会使用byName的方式注入对应的字段，不设置name属性，会默认使用byType的方式发现服务(当同一接口有多个实现的时候，必须使用byName的方式发现服务)
         helloService1.sayHello("helloService1");
 
+        // 2. 使用依赖查找的方式发现服务，主动去发现服务并使用，下面两种方式分别是byName和byType
         HelloService navigation = (HelloService) ARouter.getInstance().build("/service/hello").navigation();
-        navigation.sayHello("build");
-
         HelloService navigation2 = ARouter.getInstance().navigation(HelloService.class);
-        navigation2.sayHello("HelloService.class");
+        navigation.sayHello("byName");
+        navigation2.sayHello("byType");
     }
 
 
